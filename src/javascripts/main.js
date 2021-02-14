3f7d8911004d189a8c48c6a35b9d8c8cb0bd8498
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-02-13 19:02:43 -0600

    Update main.js

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,14 @@
+// USE WITH FIREBASE AUTH
+// import checkLoginStatus from './helpers/authentication';
+
 import '../styles/main.scss';
 
 const init = () => {
   document.querySelector('#app').innerHTML = '<h1>HELLO! You are up and running!</h1>';
   console.log('YOU ARE UP AND RUNNING!');
+  
+  // USE WITH FIREBASE AUTH
+  // checkLoginStatus();
 };
 
 init();
