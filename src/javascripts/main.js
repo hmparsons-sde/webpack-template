1cf29bdac70cb5b3e505a1c35a8be052a645e812
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-02-15 09:04:16 -0600

    removing first run errors

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,8 +5,8 @@ import '../styles/main.scss';
 
 const init = () => {
   document.querySelector('#app').innerHTML = '<h1>HELLO! You are up and running!</h1>';
-  console.log('YOU ARE UP AND RUNNING!');
-  
+  console.warn('YOU ARE UP AND RUNNING!');
+
   // USE WITH FIREBASE AUTH
   // checkLoginStatus();
 };
